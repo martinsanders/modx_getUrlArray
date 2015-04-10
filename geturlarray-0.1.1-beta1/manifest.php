580339868a2c4d5509dad2a1d89cad50a89979ec
martinsanders <martin@sandersdesign.com>
2015-04-10 12:14:15 +0100

    First commit

---
+++ geturlarray-0.1.1-beta1/manifest.php
@@ -0,0 +1,38 @@
+<?php return array (
+  'manifest-version' => '1.1',
+  'manifest-attributes' => 
+  array (
+    'readme' => 'Title: getUrlArray
+Version: 0.1.1
+By: Martin Sanders.
+
+Build array from URL string.
+
+Example:
+
+[[!getUrlArray? &parameter=`variableID` &shorten=`5`]]',
+  ),
+  'manifest-vehicles' => 
+  array (
+    0 => 
+    array (
+      'vehicle_package' => 'transport',
+      'vehicle_class' => 'xPDOObjectVehicle',
+      'class' => 'modNamespace',
+      'guid' => 'e909eb1fcf83bbf8f2df468013078d1a',
+      'native_key' => 'geturlarray',
+      'filename' => 'modNamespace/18f03b8d09c4fefcd3533e972b6e7c21.vehicle',
+      'namespace' => 'geturlarray',
+    ),
+    1 => 
+    array (
+      'vehicle_package' => 'transport',
+      'vehicle_class' => 'xPDOObjectVehicle',
+      'class' => 'modCategory',
+      'guid' => 'fab96291a105572d0387c5743467f4da',
+      'native_key' => 1,
+      'filename' => 'modCategory/19b6a4490d40e206b6628160c173c552.vehicle',
+      'namespace' => 'geturlarray',
+    ),
+  ),
+);
